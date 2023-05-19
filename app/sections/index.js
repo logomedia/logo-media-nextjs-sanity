c13b3c40e65845801d32a1a05b636f15c72e8e04
Ilyas-Shahi <ilyasm.shahi359@gmail.com>
2023-05-20 02:35:45 +0500

    services page done

--- app/sections/index.js
+++ app/sections/index.js
@@ -13,29 +13,31 @@ export { default as Faqs } from './Faqs';
 export { default as Testimonials } from './Testimonials';
 export { default as FreeAnalysis } from './FreeAnalysis';
 export { default as NewsletterSignup } from './NewsletterSignup';
+// Services page
+export { default as ServicesHero } from './ServicesHero';
+export { default as ServicesOfflineSeo } from './ServicesOfflineSeo';
+export { default as ServicesInclude } from './ServicesInclude';
+export { default as BenefitsAchieved } from './BenefitsAchieved';
+export { default as RoadMap } from './RoadMap';
+// About page
+// export { default as AboutHero } from './AboutHero'
+// export { default as OurVision } from './OurVision'
+// export { default as CoreValues } from './CoreValues'
+// export { default as AboutTeam } from './AboutTeam'
 
 // =================================================================
 
-// export { default as AboutHero } from './AboutHero'
-// export { default as AboutTeam } from './AboutTeam'
 // export { default as BackgroundMovement } from './BackgroundMovement'
-// export { default as BenefitsAchieved } from './BenefitsAchieved'
 // export { default as BestForCustomers } from './BestForCustomers'
 // export { default as BoxedImageWithText } from './BoxedImageWithText'
 // export { default as Contact } from './Contact'
-// export { default as CoreValues } from './CoreValues'
 // export { default as ELearningHero } from './ELearningHero'
 // export { default as ErrorsSection } from './ErrorsSection'
 // export { default as FavoriteDestinations } from './FavoriteDestinations'
 // export { default as Hero } from './Hero'
 // export { default as HeroLanding } from './HeroLanding'
 // export { default as OurTeachers } from './OurTeachers'
-// export { default as OurVision } from './OurVision'
 // export { default as Posts } from './Posts'
 // export { default as Pricing2 } from './Pricing2'
 // export { default as PricingTable1 } from './PricingTable1'
 // export { default as Projects } from './Projects'
-// export { default as RoadMap } from './RoadMap'
-// export { default as ServicesHero } from './ServicesHero'
-// export { default as ServicesInclude } from './ServicesInclude'
-// export { default as ServicesOfflineSeo } from './ServicesOfflineSeo'
