53a167e8b8d33dd0dcbce6afd5d40fff00ecb829
Logan Gelzer <lsgelzer7@gmail.com>
2023-06-06 16:05:41 -0400

    updates

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,5 @@
 /**
- ** This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.jsx` route
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.jsx` route
  */
 
 import { visionTool } from "@sanity/vision"
