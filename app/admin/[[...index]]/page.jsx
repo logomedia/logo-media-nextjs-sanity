493c0539bfed34b19c658891f30345550f9b0fd5
Logan Gelzer <lsgelzer7@gmail.com>
2023-06-07 13:05:01 -0400

    layout updates

--- app/admin/[[...index]]/page.jsx
+++ app/admin/[[...index]]/page.jsx
@@ -1,4 +1,5 @@
-'use client'
+"use client"
+import styles from "./admin.css"
 
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
@@ -9,9 +10,9 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import { NextStudio } from "next-sanity/studio"
+import config from "../../../sanity.config"
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+	return <NextStudio config={config} />
 }
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,14 @@
+import { getSettings } from "../lib/sanity.client"
+import { Header } from "./sections/Header"
+import { Footer } from "./sections/Footer"
+import ContextWrapper from "../app/components/ContextWrapper"
+
+import styles from "./globals.css"
+
+// slick-carousel
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 export const metadata = {
 	icons: {
 		icon: "public/icon.svg",
@@ -12,13 +23,28 @@ export const metadata = {
 		{ media: "(prefers-color-scheme: light)", color: "#540e6f" },
 		{ media: "(prefers-color-scheme: dark)", color: "#bce6f4" },
 	],
+	verification: {
+		google: "google",
+		yandex: "yandex",
+		yahoo: "yahoo",
+
+		category: "technology",
+	},
 }
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout({ children, params }) {
+	const settings = await getSettings()
+
 	return (
 		<html>
 			<body>
-				<div id='app'>{children}</div>
+				<div id='app'>
+					<ContextWrapper>
+						<Header settings={settings} />
+						{children}
+						<Footer settings={settings} />
+					</ContextWrapper>
+				</div>
 			</body>
 		</html>
 	)
