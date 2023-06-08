f12ea1f08e059ca5870afc662662ab681a32d79d
Logan Gelzer <lsgelzer7@gmail.com>
2023-06-08 13:34:13 -0400

    remove async from posts and projects sections

--- app/sections/Posts/Posts.jsx
+++ app/sections/Posts/Posts.jsx
@@ -3,13 +3,10 @@ import { Container, Unstable_Grid2 as Grid } from "@mui/material"
 //
 import PostsList from "./PostsList"
 import FeaturedPost from "./FeaturedPost"
-import { getAllPosts } from "../../../lib/sanity.client"
 
 // ----------------------------------------------------------------------
 
-export default async function Posts() {
-	const posts = await getAllPosts()
-
+export default function Posts({ posts }) {
 	return (
 		<>
 			<FeaturedPost post={posts[0]} />
