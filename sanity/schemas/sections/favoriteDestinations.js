0c7f01669b00059718d5a739c2b44006d475954f
Logan Gelzer <lsgelzer7@gmail.com>
2023-06-16 12:08:06 -0400

    rename sections in sanity

--- sanity/schemas/sections/favoriteDestinations.js
+++ sanity/schemas/sections/favoriteDestinations.js
@@ -1,63 +1,63 @@
-import { BookIcon } from '@sanity/icons'
-import { defineField, defineType } from 'sanity'
+import { BookIcon } from "@sanity/icons"
+import { defineField, defineType } from "sanity"
 
 export default defineType({
-  name: 'favoriteDestinations',
-  title: 'Our Favorite Destinations',
-  icon: BookIcon,
-  type: 'object',
+	name: "favoriteDestinations",
+	title: "4 image block with text",
+	icon: BookIcon,
+	type: "object",
 
-  fields: [
-    defineField({
-      name: 'heading',
-      type: 'string',
-      title: 'Heading',
-      validation: (rule) => rule.required(),
-    }),
+	fields: [
+		defineField({
+			name: "heading",
+			type: "string",
+			title: "Heading",
+			validation: (rule) => rule.required(),
+		}),
 
-    defineField({
-      name: 'description',
-      type: 'array',
-      title: 'Description',
-      of: [{ type: 'block' }],
-      validation: (rule) => rule.required(),
-    }),
+		defineField({
+			name: "description",
+			type: "array",
+			title: "Description",
+			of: [{ type: "block" }],
+			validation: (rule) => rule.required(),
+		}),
 
-    defineField({
-      name: 'favoritesList',
-      type: 'array',
-      title: 'Favorites List',
-      validation: (rule) => rule.required(),
-      of: [
-        {
-          name: 'listItem',
-          title: 'List Item',
-          type: 'string',
-        },
-      ],
-    }),
+		defineField({
+			name: "favoritesList",
+			type: "array",
+			title: "Favorites List",
+			validation: (rule) => rule.required(),
+			of: [
+				{
+					name: "listItem",
+					title: "List Item",
+					type: "string",
+				},
+			],
+		}),
 
-    defineField({
-      name: 'cards',
-      type: 'array',
-      title: 'Favorite Destination Cards',
-      validation: (rule) => rule.required(),
-      of: [
-        {
-          type: 'destinationCard',
-        },
-      ],
-    }),
-  ],
+		defineField({
+			name: "cards",
+			type: "array",
+			title: "Favorite Destination Cards",
+			validation: (rule) => rule.required(),
+			of: [
+				{
+					type: "destinationCard",
+				},
+			],
+		}),
+	],
 
-  preview: {
-    select: {
-      title: 'heading',
-    },
-    prepare({ title }) {
-      return {
-        title,
-      }
-    },
-  },
+	preview: {
+		select: {
+			title: "heading",
+		},
+		prepare({ title }) {
+			return {
+				title,
+			}
+		},
+	},
 })
