07d1996fe28754476eb68390d0ef740c5afc229d
lsgelzer <lsgelzer7@gmail.com>
2024-01-12 16:15:30 -0500

    sanity updates

--- app/layout.js
+++ app/layout.js
@@ -1,60 +1,61 @@
-import { getSettings } from "../lib/sanity.client"
-import { Header } from "./sections/Header"
-import { Footer } from "./sections/Footer"
-import ContextWrapper from "../app/components/ContextWrapper"
-import LoadingSkeleton from "../app/components/LoadingSkeleton"
-import styles from "./globals.css"
-import { Analytics } from "@vercel/analytics/react"
-import GTM from "../app/components/GTM"
-import { Suspense } from "react"
+import { getSettings } from "../lib/sanity.client";
+import { Header } from "./sections/Header";
+import { Footer } from "./sections/Footer";
+import ContextWrapper from "../app/components/ContextWrapper";
+import LoadingSkeleton from "../app/components/LoadingSkeleton";
+import styles from "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import GTM from "../app/components/GTM";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { draftMode } from "next/headers";
+import { token } from "../lib/sanity.fetch";
 
 // slick-carousel
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const metadata = {
-	icons: {
-		icon: "public/icon.svg",
-		shortcut: "public/icon.svg",
-		apple: "public/icon.svg",
-		other: {
-			rel: "public/icon.svg",
-			url: "public/icon.svg",
-		},
-	},
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "#540e6f" },
-		{ media: "(prefers-color-scheme: dark)", color: "#bce6f4" },
-	],
-	verification: {
-		google: "google",
-		yandex: "yandex",
-		yahoo: "yahoo",
+  icons: {
+    icon: "public/icon.svg",
+    shortcut: "public/icon.svg",
+    apple: "public/icon.svg",
+    other: {
+      rel: "public/icon.svg",
+      url: "public/icon.svg",
+    },
+  },
 
-		category: "technology",
-	},
-}
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+
+    category: "technology",
+  },
+};
+const PreviewProvider = dynamic(() => import("components/PreviewProvider"));
 
 export default async function RootLayout({ children }) {
-	const settings = await getSettings()
+  const settings = await getSettings();
 
-	return (
-		<html>
-			<body>
-				<Suspense>
-					<GTM />
-				</Suspense>
-				<div id='app'>
-					<LoadingSkeleton>
-						<ContextWrapper>
-							<Header settings={settings} />
-							{children}
-							<Footer settings={settings} />
-						</ContextWrapper>
-					</LoadingSkeleton>
-				</div>
-				<Analytics />
-			</body>
-		</html>
-	)
+  return (
+    <html>
+      <body>
+        <Suspense>
+          <GTM />
+        </Suspense>
+        <div id="app">
+          <LoadingSkeleton>
+            <ContextWrapper>
+              <Header settings={settings} />
+              {children}
+              <Footer settings={settings} />
+            </ContextWrapper>
+          </LoadingSkeleton>
+        </div>
+        <Analytics />
+      </body>
+    </html>
+  );
 }
