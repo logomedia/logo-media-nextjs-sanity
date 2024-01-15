838e572a5ea61782ac3cb347c54fba113febac2d
lsgelzer <lsgelzer7@gmail.com>
2024-01-15 15:09:48 -0500

    Sitemap Update

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,48 +1,53 @@
-import { getAllPageSlugs, getAllProjectSlugs, getAllPostSlugs, getAllServiceSlugs } from "../lib/sanity.client"
+import {
+  getAllPageSlugs,
+  getAllProjectSlugs,
+  getAllPostSlugs,
+  getAllServiceSlugs,
+} from "../lib/sanity.client";
 export default async function sitemap() {
-	const sitemap = []
-	const pages = await getAllPageSlugs()
-	const services = await getAllServiceSlugs()
-	const projects = await getAllProjectSlugs()
-	const posts = await getAllPostSlugs()
+  const sitemap = [];
+  const pages = await getAllPageSlugs();
+  const services = await getAllServiceSlugs();
+  const projects = await getAllProjectSlugs();
+  const posts = await getAllPostSlugs();
 
-	pages.forEach((page) => {
-		let object = {}
-		object.lastModified = new Date()
-		if (page.slug === "/") {
-			object.url = `https://logo.media/`
-		} else {
-			object.url = `https://logo.media/${page.slug}`
-		}
+  pages.forEach((page) => {
+    let object = {};
+    object.lastModified = new Date();
+    if (page.slug === "/") {
+      object.url = `${process.env.LIVE_DOMAIN}`;
+    } else {
+      object.url = `${process.env.LIVE_DOMAIN}/${page.slug}`;
+    }
 
-		sitemap.push(object)
-	})
-	services.forEach((page) => {
-		let object = {}
-		object.lastModified = new Date()
-		if (page.slug === "/") {
-			object.url = `https://logo.media/`
-		} else {
-			object.url = `https://logo.media/${page.slug}`
-		}
+    sitemap.push(object);
+  });
+  services.forEach((page) => {
+    let object = {};
+    object.lastModified = new Date();
+    if (page.slug === "/") {
+      object.url = `${process.env.LIVE_DOMAIN}`;
+    } else {
+      object.url = `${process.env.LIVE_DOMAIN}/${page.slug}`;
+    }
 
-		sitemap.push(object)
-	})
-	projects.forEach((page) => {
-		let object = {}
-		object.lastModified = new Date()
+    sitemap.push(object);
+  });
+  projects.forEach((page) => {
+    let object = {};
+    object.lastModified = new Date();
 
-		object.url = `https://logo.media/projects/${page.slug}`
+    object.url = `${process.env.LIVE_DOMAIN}/projects/${page.slug}`;
 
-		sitemap.push(object)
-	})
-	posts.forEach((page) => {
-		let object = {}
-		object.lastModified = new Date()
+    sitemap.push(object);
+  });
+  posts.forEach((page) => {
+    let object = {};
+    object.lastModified = new Date();
 
-		object.url = `https://logo.media/news-and-trends/${page.slug}`
+    object.url = `${process.env.LIVE_DOMAIN}/news-and-trends/${page.slug}`;
 
-		sitemap.push(object)
-	})
-	return sitemap
+    sitemap.push(object);
+  });
+  return sitemap;
 }
